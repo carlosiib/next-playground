a563f3793f5579f676419d446bc4e650a4fd5cd8
carlosiib <ntic.carlos@gmail.com>
2024-02-04 10:18:26 -0700

    handle form error with inline function -  v2

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -2,18 +2,17 @@
 import { create } from "../actions";
 
 export function Form() {
-  // v1: With wrapper function handleCreate
-  async function handleCreate(formData: FormData) {
-    const data = await create(formData)
-    console.log(data)
-    if (!data.success) {
-      alert(data?.error?.email)
-    }
-    // revalidate path and reset form
-    console.log("Form success")
-  }
+  // v2: As inline function
   return (
-    <form className="flex flex-col gap-2" action={handleCreate}>
+    <form className="flex flex-col gap-2" action={async (formData: FormData) => {
+      const data = await create(formData)
+      console.log(data)
+      if (!data.success) {
+        alert(data?.error?.email)
+      }
+      // revalidate path and reset form
+      console.log("Form success")
+    }}>
       <label className="flex flex-col gap-1">
         <span className="font-semibold">Email</span>
         <input type="email" name="email" placeholder="Enter your email" className="border p-1 rounded-md" />
